2f972a8db7e5a2b84253b091a517bb3a9f183713
kwkm2000 <kwkm.0802@gmail.com>
2023-08-06 16:26:52 +0900

    slateのコンテンツをパースする

--- src/features/articles/models/article.ts
+++ src/features/articles/models/article.ts
@@ -1,13 +1,14 @@
-import { RawDraftContentState } from "draft-js";
 import * as Tag from "../../../domain/models/tag";
+import { Descendant } from "slate";
 
 export interface Model {
   readonly id: number;
   readonly title: string;
-  readonly text: RawDraftContentState;
+  readonly text: Descendant[];
   readonly createdAt: Date;
   readonly updatedAt: Date;
   readonly tags: Tag.Model[];
+  readonly headerImage: string;
 }
 
 export type Value = Pick<Model, "title" | "text" | "tags">;
