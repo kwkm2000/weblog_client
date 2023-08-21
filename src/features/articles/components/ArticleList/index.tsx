758796ce2e2aaf27f4f6b8ccbc51a109e12f768e
kwkm2000 <kwkm.0802@gmail.com>
2023-08-21 19:51:14 +0900

    indexのデザイン修正

--- src/features/articles/components/ArticleList/index.tsx
+++ src/features/articles/components/ArticleList/index.tsx
@@ -1,27 +1,69 @@
-import React from "react";
+import React, { useCallback } from "react";
 import styles from "./index.module.css";
 import { Article } from "../../models";
-import dayjs from "dayjs";
 import Link from "next/link";
 import { formatDate } from "@/utility/formatDate";
+import Image from "next/image";
 
 interface Props {
   articles: Article.Model[];
 }
 
 const ArticleList: React.FC<Props> = ({ articles }) => {
+  const getFirstLineOFText = useCallback((article: Article.Model) => {
+    return article.text.find((text) => {
+      if (!text) {
+        return false;
+      }
+    });
+  }, []);
+  const [firstArticle, ...restArticles] = articles;
+
   return (
     <div>
-      {articles.map((article) => {
-        return (
-          <div key={article.id} style={{ marginBottom: 20 }}>
-            <p>{formatDate(article.createdAt)}</p>
-            <p>
-              <Link href={`/article/${article.id}`}>{article.title}</Link>
-            </p>
-          </div>
-        );
-      })}
+      <div className={styles.topContents}>
+        <div className={styles.topContentsImageWrapper}>
+          <Image
+            alt=""
+            fill={true}
+            src={firstArticle.headerImage}
+            className={styles.topContentsImage}
+          />
+        </div>
+
+        <div className="text">
+          <p>{formatDate(firstArticle.createdAt)}</p>
+          <p>
+            <Link href={`/article/${firstArticle.id}`}>
+              {firstArticle.title}
+            </Link>
+          </p>
+        </div>
+      </div>
+      {restArticles.length && (
+        <div className={styles.articlesContainer}>
+          {restArticles.map((article) => {
+            return (
+              <div key={article.id} className={styles.articleListColumn}>
+                <Link href={`/article/${article.id}`}>
+                  {!!article.headerImage && (
+                    <div className={styles.articleListImageWrapper}>
+                      <Image
+                        alt=""
+                        fill={true}
+                        src={article.headerImage}
+                        className={styles.articleListImage}
+                      />
+                    </div>
+                  )}
+                  <p>{article.title}</p>
+                  <p>{formatDate(article.createdAt)}</p>
+                </Link>
+              </div>
+            );
+          })}
+        </div>
+      )}
     </div>
   );
 };
